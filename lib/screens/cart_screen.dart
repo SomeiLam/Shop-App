48c316abdfbe4ae51872d1571a188fca6356762e
So Mei Amy Lam <lamsomeiamy@gmail.com>
2023-01-16 19:26:05 -0800

    add order request

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -40,25 +40,7 @@ class CartScreen extends StatelessWidget {
                     ),
                     Padding(
                       padding: EdgeInsets.only(left: 5),
-                      child: ElevatedButton(
-                          child: Text('ORDER NOW'),
-                          onPressed: () {
-                            Provider.of<Orders>(context, listen: false)
-                                .addOrder(
-                              cart.items.values.toList(),
-                              cart.totalAmount,
-                            );
-                            cart.clear();
-                          },
-                          style: ElevatedButton.styleFrom(
-                            foregroundColor:
-                                Theme.of(context).colorScheme.primary,
-                            backgroundColor: Colors.white,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(5),
-                            ),
-                            elevation: 0, // shadow
-                          )),
+                      child: OrderButton(cart: cart),
                     )
                   ]),
             )),
@@ -76,5 +58,50 @@ class CartScreen extends StatelessWidget {
         ))
       ]),
     );
+  }
+}
+
+class OrderButton extends StatefulWidget {
+  const OrderButton({
+    Key key,
+    @required this.cart,
+  }) : super(key: key);
+
+  final Cart cart;
+
+  @override
+  State<OrderButton> createState() => _OrderButtonState();
+}
+
+class _OrderButtonState extends State<OrderButton> {
+  var _isLoading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        child: _isLoading ? CircularProgressIndicator() : Text('ORDER NOW'),
+        onPressed: (widget.cart.totalAmount <= 0 || _isLoading)
+            ? null
+            : () async {
+                setState(() {
+                  _isLoading = true;
+                });
+                await Provider.of<Orders>(context, listen: false).addOrder(
+                  widget.cart.items.values.toList(),
+                  widget.cart.totalAmount,
+                );
+                setState(() {
+                  _isLoading = false;
+                });
+                widget.cart.clear();
+              },
+        style: ElevatedButton.styleFrom(
+          foregroundColor: Theme.of(context).colorScheme.primary,
+          backgroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(5),
+          ),
+          elevation: 0, // shadow
+        ));
   }
 }
